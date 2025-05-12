a16ce66051745a65108cb205c3f933bb3a134723
Lakshay <lakshayyg09@gmail.com>
2025-05-12 15:45:00 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,28 @@
+# creating strings
+
+name1 = 'me'
+name2 = "Kashmir"
+name3 = '''us'''
+
+# print(name1,name2,name3)
+
+# print(type(name1))
+# print(type(name2))
+# print(type(name3))
+
+# indexing in a string
+# print(name2[1])
+# print(name2[-1])
+
+# traversing a string
+# for i in name2:
+#     print(i)
+
+# find char/sub string in string
+# print(name2.find("o")) 
+
+# slicing a string i used to get part of a string
+# print(name2[1:5])
+# print(name2[:3])
+# print(name2[3:])
+
