90471007fc7ee6d71a5060706a739198c71331ea
Lakshay <lakshayyg09@gmail.com>
2025-05-12 15:52:11 +0530

    Add files via upload

---
+++ modifying_strings.py
@@ -0,0 +1,42 @@
+# converting char to upper case
+# str1 = "kashmir"
+# str2 = str1.upper()
+# print(str2)
+
+# converting char to lower case
+# str1 = "KASHMIR"
+# str2 = str1.lower()
+# print(str2)
+
+# converting first char to sting in upper case
+# str1 = "kashmir"
+# str2 = str1.capitalize()
+# print(str2)
+
+# for removng any trailing space
+# str1 = "            kashmir          "
+# print(str1.strip())
+# print(str1)
+
+
+# replacings substring
+# str1 = "Hello World, World"
+# print(str1.replace("World","Earth", 1))
+
+# splliting string
+# str1 = "ria sia tia mia jia"
+# list = str1.split()   # Gadbad
+# print(list)
+
+# concatenation
+# str1 = "hello"
+# str2 = "world"
+# print(str1 + str2)
+
+# fomrat (used to variable value in a string)
+# student_name = 'tahir'
+# student_marks = 98
+
+# str1 = 'the student name is {s} & marks is {m}'.format(s= student_name, m = student_marks)
+# print(str1)
+
