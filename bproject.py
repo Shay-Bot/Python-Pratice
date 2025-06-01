e43851885a91d00aa9ae374e8a24515b356e1b6e
Lakshay <lakshayyg09@gmail.com>
2025-06-01 07:34:15 +0530

    Update slot-machine-project.py

--- bproject.py
+++ bproject.py
@@ -1,6 +1,48 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 1000
 MIN_BET = 10
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_Slot_machine_spin(rows, cols, sybmols):
+    all_symbols = []
+    for symbol, symbol_count in sybmols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+def print_slot_machine(coloums):
+    for row in range(len(coloums[0])):
+        for i, coloum in enumerate(coloums):
+            if i != len(coloums) - 1:
+                print(coloum[row], end=" | ")
+            else:
+                print(coloum[row], end="")
+
+        print()
+
 
 def deposit():
     while True:
@@ -32,7 +74,7 @@ def get_number_of_lines():
 
 def get_bet():
     while True:
-        amount = input("What would you like to Bet? $")
+        amount = input("How much would you like to Bet? $")
         if amount.isdigit():
             amount = int(amount)
             if MIN_BET <= amount <= MAX_BET:
@@ -60,5 +102,8 @@ def main():
             break
 
     print(f"you are betting ${bet} on {lines}. Total bet is equal to: ${total_bet} ")
+
+    slots = get_Slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 main()
